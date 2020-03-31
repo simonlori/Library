f5151e3c98db6d9adff1253307e0af99c0d83ded
Lori <ss_lori@yahoo.com>
2020-03-31 14:42:15 +0300

    usersupdated

--- src/library/Interface.java
+++ src/library/Interface.java
@@ -159,11 +159,11 @@ public class Interface extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 623, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addGap(0, 286, Short.MAX_VALUE)
         );
 
         pack();
